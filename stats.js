d8b929aff3b0f7d0212090ff40b1dbe81a329c4f
omansh <omanshdhawan45@gmail.com>
2021-08-09 01:20:24 +0530

    done

--- stats.js
+++ stats.js
@@ -4,6 +4,8 @@ let path=require("path");
 let request=require("request");
 let cheerio=require("cheerio");
 
+
+
 function processSinglematch(url){
     request(url,cb3);
 
@@ -28,12 +30,22 @@ function getStats(html){
     }
 
     let teams=searchTool(".Collapsible");
+    let detail=searchTool(".description").text().split(",");
+    venu=detail[1].trim();
+    let date=detail[2].trim();
+    let result=searchTool(".match-info span");
+    result=searchTool(result[result.length-1]).text();
     
-    //team1 folder
+    
+    //team1 Name
     let t1=searchTool(teams[0]).text();
     t1Name=t1.split("INNINGS")[0].trim();
 
+    //team2 Name
+    let t2=searchTool(teams[1]).text();
+    t2Name=t2.split("INNINGS")[0].trim();
     
+    //team1 folder
     let t1Folder=path.join(folderPath,t1Name);
     if(fs.existsSync(t1Folder)==false){
         fs.mkdirSync(t1Folder);
@@ -41,24 +53,35 @@ function getStats(html){
 
     //t1 players->batsman files
 
-    let t1Players=searchTool(teams[0]).find(".table.batsman tbody tr .batsman-cell");
+    let t1Players=searchTool(teams[0]).find(".table.batsman tbody tr");
     
     for(let i=0;i<t1Players.length;i++){
-        let playerName=searchTool(t1Players[i]).find("a").text();
-        let location=path.join(t1Folder,playerName);
+        let noOfTd=searchTool(t1Players[i]).find("td");
+        
+        let opponentTeamName=t2Name;
+
+        if(noOfTd.length==8){
+            let playerName=searchTool(noOfTd[0]).text();
+            let runs=searchTool(noOfTd[2]).text();
+            let balls=searchTool(noOfTd[3]).text();
+            let fours=searchTool(noOfTd[5]).text();
+            let sixes=searchTool(noOfTd[6]).text();
+            let sr=searchTool(noOfTd[7]).text();
+
+            let location=path.join(t1Folder,playerName+".json");
+
+        let jsonData=contentRead(t1Name,playerName,venu,date,opponentTeamName,result,runs,balls,fours,sixes,sr);
+        let jsonWrite=JSON.stringify(jsonData);
+        
 
         if(fs.existsSync(location)==false){
-            fs.writeFileSync(location,"done");
+            fs.writeFileSync(location,jsonWrite+",\n");
+        }else{
+            fs.appendFileSync(location,jsonWrite+",\n");
         }
-            
 
+        }
     }
-
-    //team2 folder
-    let t2=searchTool(teams[1]).text();
-    t2Name=t2.split("INNINGS")[0].trim();
-    
-    
     let t2Folder=path.join(folderPath,t2Name);
     if(fs.existsSync(t2Folder)==false){
         fs.mkdirSync(t2Folder);
@@ -66,19 +89,53 @@ function getStats(html){
 
     //t2 players->batsman files
 
-    let t2Players=searchTool(teams[1]).find(".table.batsman tbody tr .batsman-cell");
+    let t2Players=searchTool(teams[1]).find(".table.batsman tbody tr");
     for(let i=0;i<t2Players.length;i++){
-        let playerName=searchTool(t2Players[i]).find("a").text();
-
-        let location=path.join(t2Folder,playerName);
+        let noOfTd=searchTool(t2Players[i]).find("td");
         
+        let opponentTeamName=t1Name;
+        
+        
+        if(noOfTd.length==8){
+            let playerName=searchTool(noOfTd[0]).text();
+            let runs=searchTool(noOfTd[2]).text();
+            let balls=searchTool(noOfTd[3]).text();
+            let fours=searchTool(noOfTd[5]).text();
+            let sixes=searchTool(noOfTd[6]).text();
+            let sr=searchTool(noOfTd[7]).text();
+            let location=path.join(t2Folder,playerName+".json");
+
+        let jsonData=contentRead(t2Name,playerName,venu,date,opponentTeamName,result,runs,balls,fours,sixes,sr);
+        let jsonWrite=JSON.stringify(jsonData);
+
         if(fs.existsSync(location)==false){
-            fs.writeFileSync(location,"done");
+            fs.writeFileSync(location,jsonWrite+",\n");
+        }else{
+            fs.appendFileSync(location,jsonWrite+",\n");
         }
 
+        }
+        
     }
-
+}
+function contentRead(t1Name,playerName,venu,date,opponentTeamName,result,runs,balls,fours,sixes,sr){
+    let data={
+        "myTeamName":t1Name,
+        "name":playerName,
+        "venu":venu ,
+        "date":date,
+        "opponentTeamName":opponentTeamName,
+        "result":result,
+        "runs":runs,
+        "balls":balls,
+        "fours":fours,
+        "sixes":sixes,
+        "sr":sr
+    }
+    
+    return data;
 }
 module.exports={
     processSinglematch
 }
+    
